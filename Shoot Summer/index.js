7995ccede27d533f2829c1867b039de7fa7d7443
lorashopova <lshopowa@yahoo.com>
2017-06-25 02:34:58 +0300

    Web Application Node.js
    Web Application Node.js

---
+++ Shoot Summer/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use('/static', express.static('public'));
+app.use('/libs', express.static('node_modules'));
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('home/home');
+});
+
+app.get('/home', (req, res) => {
+     res.render('home/home');
+});
+
+app.get('/explore', (req, res) => {
+     res.render('explore/explore');
+});
+
+app.listen(3002, () => console.log('Server listen on :3002'));
