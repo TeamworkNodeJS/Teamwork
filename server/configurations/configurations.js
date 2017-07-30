ac78924d916834ebee18c39f7eed14bc21a49dee
lorashopova <lshopowa@yahoo.com>
2017-07-30 21:27:59 +0300

    mongodb connection changed

--- server/configurations/configurations.js
+++ server/configurations/configurations.js
@@ -2,10 +2,10 @@
 
 // eslint-disable-next-line no-process-env
 const port = process.env.PORT || 3001;
-// const connectionString = 'mongodb://localhost:27017/summer';
+const connectionString = 'mongodb://localhost:27017/summer';
 const sessionSecret = 'pink cat';
 
-const connectionString = 'mongodb://Admin:admin@ds143532.mlab.com:43532/summer';
+// const connectionString = 'mongodb://Admin:admin@ds143532.mlab.com:43532/summer';
 
 module.exports = {
     port, connectionString, sessionSecret,
