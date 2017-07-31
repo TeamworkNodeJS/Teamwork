227f6fc6c3cfe52d5aa6df474d3d07b97a13364d
lorashopova <lshopowa@yahoo.com>
2017-07-31 14:34:21 +0000

    files from ubuntu added
     Changes to be committed:
    	modified:   server/configurations/configurations.js
    	new file:   start.sh
    	modified:   yarn.lock

--- server/configurations/configurations.js
+++ server/configurations/configurations.js
@@ -1,11 +1,14 @@
 /* globals process */
 
+
 // eslint-disable-next-line no-process-env
-const port = process.env.PORT || 3001;
-// const connectionString = 'mongodb://localhost:27017/summer';
+const port = 80;
+
+const connectionString = 'mongodb://ec2-18-220-65-226.us-east-2.compute.amazonaws.com:27017/summer';
+
 const sessionSecret = 'pink cat';
 
-const connectionString = 'mongodb://Admin:admin@ds143532.mlab.com:43532/summer';
+//const connectionString = 'mongodb://Admin:admin@ds143532.mlab.com:43532/summer';
 
 module.exports = {
     port, connectionString, sessionSecret,
